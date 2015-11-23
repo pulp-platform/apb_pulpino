4ede051323d91b3a41adda3aff7b158c01e5fabf
Flo Zaruba <fl.zaruba@be4web.net>
2015-11-24 00:45:29 +0100

    Adaption to pad mux - same as GPIO reg map (not very satisfying imo)

--- apb_pulpino.sv
+++ apb_pulpino.sv
@@ -1,15 +1,18 @@
 // Address offset: bit [4:2]
-`define REG_PAD_MUX      3'b000
-`define REG_PAD_CONF     3'b001
-`define REG_INFO         3'b010
+`define REG_PAD_MUX      4'b0000
+`define REG_INFO         4'b0100
 
-`define REG_PADCFG0     5'b00000 //BASEADDR
-`define REG_PADCFG1     5'b00101 //BASEADDR+0x05
-`define REG_PADCFG2     5'b01010 //BASEADDR+0x0A
-`define REG_PADCFG3     5'b01111 //BASEADDR+0x0F
-`define REG_PADCFG4     5'b10100 //BASEADDR+0x14
-`define REG_PADCFG5     5'b11001 //BASEADDR+0x30
+// GPIO Map - for simplicity
+`define REG_PADCFG0     4'b1000 //BASEADDR+0x20
+`define REG_PADCFG1     4'b1001 //BASEADDR+0x24
+`define REG_PADCFG2     4'b1010 //BASEADDR+0x28
+`define REG_PADCFG3     4'b1011 //BASEADDR+0x2C
+`define REG_PADCFG4     4'b1100 //BASEADDR+0x30
+`define REG_PADCFG5     4'b1101 //BASEADDR+0x34
+`define REG_PADCFG6     4'b1110 //BASEADDR+0x38
+`define REG_PADCFG7     4'b1111 //BASEADDR+0x3C
 
+// info reg
 `define VERSION          5'b00001 // Version number 1.0
 `define DATA_RAM         8'b00000100 //size of data ram in multiples of 8 kBye
 `define INSTR_RAM        8'b00000100 //size of instr ram in multiples of 8 kBye
@@ -41,7 +44,7 @@ module apb_pulpino
 
     logic [APB_ADDR_WIDTH - 1:0]       register_adr;
 
-    assign register_adr = PADDR[4:2];
+    assign register_adr = PADDR[5:2];
 
     // directly output registers to pad frame
     assign pad_mux_o = pad_mux_q;
@@ -59,16 +62,65 @@ module apb_pulpino
             case (register_adr)
                 `REG_PAD_MUX:
                 begin
-                    pad_mux_n[0] = PWDATA[`REG_PADCFG0];
-                    pad_mux_n[1] = PWDATA[`REG_PADCFG1];
-                    pad_mux_n[2] = PWDATA[`REG_PADCFG2];
-                    pad_mux_n[3] = PWDATA[`REG_PADCFG3];
-                    pad_mux_n[4] = PWDATA[`REG_PADCFG4];
-                    pad_mux_n[5] = PWDATA[`REG_PADCFG5];
+                    pad_mux_n     = PWDATA;
                 end
 
-                `REG_PAD_CONF:
-                    pad_cfg_n[PADDR[9:5]] = PWDATA;
+                `REG_PADCFG0:
+                begin
+                    pad_cfg_n[0]  = PWDATA[4:0];
+                    pad_cfg_n[1]  = PWDATA[12:8];
+                    pad_cfg_n[2]  = PWDATA[20:16];
+                    pad_cfg_n[3]  = PWDATA[28:24];
+                end
+                `REG_PADCFG1:
+                begin
+                    pad_cfg_n[4]  = PWDATA[4:0];
+                    pad_cfg_n[5]  = PWDATA[12:8];
+                    pad_cfg_n[6]  = PWDATA[20:16];
+                    pad_cfg_n[7]  = PWDATA[28:24];
+                end
+                `REG_PADCFG2:
+                begin
+                    pad_cfg_n[8]  = PWDATA[4:0];
+                    pad_cfg_n[9]  = PWDATA[12:8];
+                    pad_cfg_n[10] = PWDATA[20:16];
+                    pad_cfg_n[11] = PWDATA[28:24];
+                end
+                `REG_PADCFG3:
+                begin
+                    pad_cfg_n[12] = PWDATA[4:0];
+                    pad_cfg_n[13] = PWDATA[12:8];
+                    pad_cfg_n[14] = PWDATA[20:16];
+                    pad_cfg_n[15] = PWDATA[28:24];
+                end
+                `REG_PADCFG4:
+                begin
+                    pad_cfg_n[16] = PWDATA[4:0];
+                    pad_cfg_n[17] = PWDATA[12:8];
+                    pad_cfg_n[18] = PWDATA[20:16];
+                    pad_cfg_n[19] = PWDATA[28:24];
+                end
+                `REG_PADCFG5:
+                begin
+                    pad_cfg_n[20] = PWDATA[4:0];
+                    pad_cfg_n[21] = PWDATA[12:8];
+                    pad_cfg_n[22] = PWDATA[20:16];
+                    pad_cfg_n[23] = PWDATA[28:24];
+                end
+                `REG_PADCFG6:
+                begin
+                    pad_cfg_n[24] = PWDATA[4:0];
+                    pad_cfg_n[25] = PWDATA[12:8];
+                    pad_cfg_n[26] = PWDATA[20:16];
+                    pad_cfg_n[27] = PWDATA[28:24];
+                end
+                `REG_PADCFG7:
+                begin
+                    pad_cfg_n[28]  = PWDATA[4:0];
+                    pad_cfg_n[29]  = PWDATA[12:8];
+                    pad_cfg_n[30]  = PWDATA[20:16];
+                    pad_cfg_n[31]  = PWDATA[28:24];
+                end
 
                 // version reg can't be written to
             endcase
@@ -88,12 +140,33 @@ module apb_pulpino
                 `REG_PAD_MUX:
                     PRDATA = pad_mux_q;
 
-                `REG_PAD_CONF:
-                    // leave some spaces for future pad technology changes...
-                    PRDATA = {6'b0, pad_cfg_q[PADDR[9:5]][5], 4'b0, pad_cfg_q[PADDR[9:5]][4], 4'b0, pad_cfg_q[PADDR[9:5]][3], 4'b0, pad_cfg_q[PADDR[9:5]][2], 4'b0, pad_cfg_q[PADDR[9:5]][1], 4'b0, pad_cfg_q[PADDR[9:5]][0]};
+                `REG_PADCFG0:
+                    PRDATA = {2'b00,pad_cfg_q[3],2'b00,pad_cfg_q[2],2'b00,pad_cfg_q[1],2'b00,pad_cfg_q[0]};
+
+                `REG_PADCFG1:
+                    PRDATA = {2'b00,pad_cfg_q[7],2'b00,pad_cfg_q[6],2'b00,pad_cfg_q[5],2'b00,pad_cfg_q[4]};
+
+                `REG_PADCFG2:
+                    PRDATA = {2'b00,pad_cfg_q[11],2'b00,pad_cfg_q[10],2'b00,pad_cfg_q[9],2'b00,pad_cfg_q[8]};
+
+                `REG_PADCFG3:
+                    PRDATA = {2'b00,pad_cfg_q[15],2'b00,pad_cfg_q[14],2'b00,pad_cfg_q[13],2'b00,pad_cfg_q[12]};
+
+                `REG_PADCFG4:
+                    PRDATA = {2'b00,pad_cfg_q[19],2'b00,pad_cfg_q[18],2'b00,pad_cfg_q[17],2'b00,pad_cfg_q[16]};
+
+                `REG_PADCFG5:
+                    PRDATA = {2'b00,pad_cfg_q[23],2'b00,pad_cfg_q[22],2'b00,pad_cfg_q[21],2'b00,pad_cfg_q[20]};
+
+                `REG_PADCFG6:
+                    PRDATA = {2'b00,pad_cfg_q[27],2'b00,pad_cfg_q[26],2'b00,pad_cfg_q[25],2'b00,pad_cfg_q[24]};
+
+                `REG_PADCFG7:
+                    PRDATA = {2'b00,pad_cfg_q[31],2'b00,pad_cfg_q[30],2'b00,pad_cfg_q[29],2'b00,pad_cfg_q[28]};
 
                 `REG_INFO:
                     PRDATA = {4'b0000, `DCACHE, `ICACHE, `ROM, `INSTR_RAM, `DATA_RAM,`VERSION};
+
                 default:
                     PRDATA = 'b0;
             endcase
@@ -106,8 +179,8 @@ module apb_pulpino
     begin
         if(~HRESETn)
         begin
-            pad_mux_q          <= '{default: 32'b0};
-            
+            pad_mux_q          <= 32'b0;
+            pad_cfg_q          <= '{default: 32'b0};
             // cfg_pad_int[i][0]: PD, Pull Down
             // cfg_pad_int[i][1]: PU, Pull Up
             // cfg_pad_int[i][2]: SMT, Schmitt Trigger
