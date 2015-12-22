441ecefb048dec775a989e408c0512907bc1cc25
Andreas Traber <andytraber@gmail.com>
2015-12-22 09:18:25 +0100

    Add license

--- apb_pulpino.sv
+++ apb_pulpino.sv
@@ -1,3 +1,13 @@
+// Copyright 2015 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the “License”); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+
 // Address offset: bit [4:2]
 `define REG_PAD_MUX      4'b0000
 `define REG_CLK_GATE     4'b0001
@@ -40,7 +50,7 @@ module apb_pulpino
     output logic                      PSLVERR,
 
     output logic         [31:0] [5:0] pad_cfg_o,
-    output logic               [31:0] clk_gate_o, 
+    output logic               [31:0] clk_gate_o,
     output logic               [31:0] pad_mux_o,
     output logic               [31:0] boot_addr_o
 );
